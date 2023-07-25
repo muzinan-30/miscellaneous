0b3ec770238a86e1ca0a2c037989d7b3a942ace1
muzinan <86657172+muzinan-30@users.noreply.github.com>
2023-07-25 22:52:17 +0800

    Update PCB.cc
    nothing

--- linux_basic/PCB.cc
+++ linux_basic/PCB.cc
@@ -6,7 +6,8 @@
 #include<sys/wait.h>
 //pcb的本质是一个task_struct pid（进程标识符，父进程标识符，用户标识符，组标识符）进程组，进程环境，进程的运行状态等 
 using namespace std;
-
+//fork() 子进程创建成功时 父进程的fork返回0， 子进程的fork不返回
+//fork创建失败时 父进程返回－1
 int main(){
     pid_t p1=getpid();
     pid_t p2=getppid();
@@ -29,7 +30,7 @@ int main(){
         // execl("/bin/ls","-a","-l","PCB.cc",NULL);
         // perror("error exec\n");
         p=child1;//获取第一个子进程的id
-        //父进程继续创建子进程
+        //父进程继续创建子进程 保证是父进程创建的子进程 (pid＝fork())==0
         for(int i=0;i<3;i++){
             if((pid=fork())==0){
                 break;
